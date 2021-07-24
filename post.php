e1b60669804a5ea39058ee9b63572e3abfe46d34
Emily Chamberlain <73752639+echamberlain2019@users.noreply.github.com>
2021-07-24 12:37:00 -0400

    Update post.php

--- post.php
+++ post.php
@@ -14,20 +14,22 @@
 </head>
 <body>
 
-<?php 
- include 'main.php';
- $check  = new Main;
- $get    = new Main;
- $send   = new Main;
+<?php session_start();?>
+<?php ## if(!empty($_SESSION['user_id'])): you can use this instead when user_id is created ?>
+<?php if(!empty($_SESSION['login_user']['Username'])): ?>
+User (<?=$USER['Username']?>) is logged in. <a href="<?=$ROUTES['logout']?>" >Logout (<?=$_SESSION['login_user']['Username']?>)</a>
+<?php else: ?>
+   User not logged in.
+<?php endif; ?>
    @$user_id = $_SESSION['user_id'];
- $data  = $get->user_data($user_id);
- $post  = $get->posts();
- if(isset($_POST['submit'])){
-  $status  = $_POST['status'];
+   $data  = $get->user_data($user_id);
+  $post  = $get->posts();
+  if(isset($_POST['submit'])){
+      $status  = $_POST['status'];
   if (isset($_FILES['post_image'])===true) {
-    if (empty($_FILES['post_image']['name']) ===true) {
-    if(!empty($status)===true){
-     $send->add_post($user_id,$status);
+      if (empty($_FILES['post_image']['name']) ===true) {
+      if(!empty($status)===true){
+         $send->add_post($user_id,$status);
     }
       }else {                                                                                                      
      $allowed = array('jpg','jpeg','gif','png'); 
@@ -148,130 +150,3 @@
   }
 ?>
 
-<html>
- <head>
-  <title>Post on Covid Hut</title>
-  <link rel="stylesheet" href="css/style.css"/>
-  <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-
- </head>
-<body>
-<div class="head">
- <div class="head-in">
-  <diV class="head-logo">
-   <h1 class="h-1"></h1>
-  </div><?php 
-   if($check->logged_in() === false){
-    include 'login.php';
-   } 
-  ?></div>
-</div>
-
-
-
- <div class="wrapper">  
-  <div class="content">
-   <div class="center">
-    <div class="posts">
-     <div class="create-posts">
-      <form action="" method="post" enctype="multipart/form-data">
-      <div class="c-header">
-       <div class="c-h-inner">
-        <ul> 
-         <li style="border-right:none;"><img src="img/icon3.png"></img><a href="#">Update Status</a></li>
-         <li><input type="file"  onchange="readURL(this);" style="display:none;" name="post_image" id="uploadFile"></li>
-         <li><img src="img/icon1.png"></img><a href="#" id="uploadTrigger" name="post_image">Add Photos/Video</a></li>
-        </ul>
-       </div>
-      </div>
-      <div class="c-body">
-       <div class="body-left">
-        <div class="img-box">
-         <img src="<?php echo $data['profile_image'];?>"></img>
-         
-        </div>
-       </div>
-       <div class="body-right">
-        <textarea class="text-type" name="status" placeholder="What's on your mind?"></textarea>
-       </div>
-       <div id="body-bottom">
-       <img src="#"  id="preview"/>
-       </div>
-      </div>
-      <div class="c-footer">
-       <div class="right-box">
-        <ul>
-         <li><button class="btn1"><img class="iconw-margin" src="img/iconw.png"></img>Public<img class="iconp-margin" src="img/iconp.png"></img></button></li>
-         <li><input type="submit" name="submit" value="Post" class="btn2"/></li>
-        </ul>
-       </div>
-        
-       </div>
-      </div>
-      </div>
-  <script type="text/javascript">
-        $("#uploadTrigger").click(function(){
-           $("#uploadFile").click();
-        });
-              function readURL(input) {
-                  if (input.files && input.files[0]) {
-                      var reader = new FileReader();
-
-                      reader.onload = function (e) {
-                       $('#body-bottom').show();
-                          $('#preview').attr('src', e.target.result);
-                      }
-
-                      reader.readAsDataURL(input.files[0]);
-                  }
-              }
-
-        </script>
-<?php foreach($post as $row){
-       $time_ago = $row['status_time'];
-      echo '
-      <div class="post-show">
-         <div class="post-show-inner">
-          <div class="post-header">
-           <div class="post-left-box">
-            <div class="id-img-box"><img src="'.$row['profile_image'].'"></img></div>
-            <div class="id-name">
-             <ul>
-              <li><a href="#">'.$row['username'].'</a></li>
-              <li><small>'.$get->timeAgo($time_ago).'</small></li>
-             </ul>
-            </div>
-           </div>
-           <div class="post-right-box"></div>
-          </div>
-         
-           <div class="post-body">
-           <div class="post-header-text">
-            '.$row['status'].'
-           </div>'.( ($row['status_image'] != 'NULL') ? '<div class="post-img">
-            <img src="'.$row['status_image'].'"></img></div>' : '').'
-           <div class="post-footer">
-            <div class="post-footer-inner">
-             <ul>
-              <li><a href="#">Like</a></li>
-              <li><a href="#">Comment</a></li>
-              <li><a href="#">Share</a></li>
-             </ul> 
-            </div>
-           </div>
-          </div>
-         </div>
-        </div><br> ';  
-     } 
-    ?>
-        </div>
-     </form> 
-             
-   </div>
-
- 
-
-
-
-</body>
-</html>
