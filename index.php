391862f43a428de66cdf4fbaa655b2e62b9a7bfd
Emily Chamberlain <73752639+echamberlain2019@users.noreply.github.com>
2021-07-24 12:45:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -47,64 +47,117 @@
   <a href="#">About</a>
 </nav>
 
-<div class="container">
-	<div class="row">
-		<div class="Carousel" data-items="1,3,5,6" data-slide="1" id="Carousel"  data-interval="1000">
-            <div class="Carousel-inner">
-                <div class="item">
-                    <div class="post">
-                        <p class="title">Multi Item Carousel</p>
-                        <p></p>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="post">
-                        <p class="title">Multi Item Carousel</p>
-                        <p></p>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="post">
-                        <p class="title">Multi Item Carousel</p>
-                        <p></p>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="post">
-                        <p class="title">Multi Item Carousel</p>
-                        <p></p>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="post">
-                        <p class="title">Multi Item Carousel</p>
-                        <p></p>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="post">
-                        <p class="title">Multi Item Caurosel</p>
-                        <p></p>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="post">
-                        <p class="title">Multi Item Carousel</p>
-                        <p></p>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="post">
-                        <p class="title">Multi Item Carousel</p>
-                        <p></p>
-                    </div>
-                </div>
-               
-            </div>
-            <button class="btn btn-primary left"><</button>
-            <button class="btn btn-primary right">></button>
+<div class="head">
+ <div class="head-in">
+  <diV class="head-logo">
+   <h1 class="h-1"></h1>
+  </div><?php 
+   if($check->logged_in() === false){
+    include 'login.php';
+   } 
+  ?></div>
+</div>
+
+ <div class="wrapper">  
+  <div class="content">
+   <div class="center">
+    <div class="posts">
+     <div class="create-posts">
+      <form action="" method="post" enctype="multipart/form-data">
+      <div class="c-header">
+       <div class="c-h-inner">
+        <ul> 
+         <li style="border-right:none;"><img src="assets/icon3.png"></img><a href="#">Update Status</a></li>
+         <li><input type="file"  onchange="readURL(this);" style="display:none;" name="post_image" id="uploadFile"></li>
+         <li><img src="assets/icon1.jpeg"></img><a href="#" id="uploadTrigger" name="post_image">Add Photos/Video</a></li>
+        </ul>
+       </div>
+      </div>
+      <div class="c-body">
+       <div class="body-left">
+        <div class="img-box">
+         <img src="<?php echo $data['profile_image'];?>"></img>
+         
         </div>
-	</div>
+       </div>
+       <div class="body-right">
+        <textarea class="text-type" name="status" placeholder="What's on your mind?"></textarea>
+       </div>
+       <div id="body-bottom">
+       <img src="#"  id="preview"/>
+       </div>
+      </div>
+      <div class="c-footer">
+       <div class="right-box">
+        <ul>
+         <li><button class="btn1"><img class="iconw-margin" src="assets/sent.png"></img>Public<img class="iconp-margin" src="assets/sent.png"></img></button></li>
+         <li><input type="submit" name="submit" value="Post" class="btn2"/></li>
+        </ul>
+       </div>
+        
+       </div>
+      </div>
+      </div>
+  <script type="text/javascript">
+        $("#uploadTrigger").click(function(){
+           $("#uploadFile").click();
+        });
+              function readURL(input) {
+                  if (input.files && input.files[0]) {
+                      var reader = new FileReader();
+
+                      reader.onload = function (e) {
+                       $('#body-bottom').show();
+                          $('#preview').attr('src', e.target.result);
+                      }
+
+                      reader.readAsDataURL(input.files[0]);
+                  }
+              }
+
+        </script>
+<?php foreach($post as $row){
+       $time_ago = $row['status_time'];
+      echo '
+      <div class="post-show">
+         <div class="post-show-inner">
+          <div class="post-header">
+           <div class="post-left-box">
+            <div class="id-img-box"><img src="'.$row['profile_image'].'"></img></div>
+            <div class="id-name">
+             <ul>
+              <li><a href="#">'.$row['username'].'</a></li>
+              <li><small>'.$get->timeAgo($time_ago).'</small></li>
+             </ul>
+            </div>
+           </div>
+           <div class="post-right-box"></div>
+          </div>
+         
+           <div class="post-body">
+           <div class="post-header-text">
+            '.$row['status'].'
+           </div>'.( ($row['status_image'] != 'NULL') ? '<div class="post-img">
+            <img src="'.$row['status_image'].'"></img></div>' : '').'
+           <div class="post-footer">
+            <div class="post-footer-inner">
+             <ul>
+              <li><a href="#">Like</a></li>
+              <li><a href="#">Comment</a></li>
+              <li><a href="#">Share</a></li>
+             </ul> 
+            </div>
+           </div>
+          </div>
+         </div>
+        </div><br> ';  
+     } 
+    ?>
+        </div>
+     </form> 
+             
+   </div>
+
 
 <div class="container-fluid bg-1 text-center">
   <div class="col-sm-5">
