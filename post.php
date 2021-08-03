5c7237f04399963bb56d5e296f83a7b2d667e3aa
Jose Abel Garcia <garciabel212@gmail.com>
2021-08-02 23:13:53 -0400

    post page added auth , posting, comments and likes

--- post.php
+++ post.php
@@ -1,81 +1,73 @@
 <?php
- include_once('./php-inc/config.php');
- include_once('./php-inc/config_routes.php');
- session_start();
- $USER = $_SESSION[$USER_SESSION_KEY] ?? null;
-   if($USER){
-      $ses_sql = mysqli_query($db,"SELECT * FROM Users WHERE Username='".$USER['Username']."'");
-      $count = $ses_sql->num_rows;
-      echo $count;
-      $Username = $ses_sql;
-      echo $Username;
-      $rows = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
-      if(!$rows){
-         $USER = null;
-         echo 'could not find user';
-      }
-   }
-
-   if(!$USER){
-      echo 'No user';
-      header("location:".$ROUTES['login']);
-      die();
-     }
-
-
-
- class  Main{
-    function login($USER,$Password){
-      global $pdo;
-      $query = $pdo->prepare("SELECT * from Users WHERE Username = ? and Password = ?");
-      $query->bindValue(1,$Username);
-      $query->bindValue(2,$Password);
-      $query->execute();
-         $rows = $query->fetch(PDO::FETCH_NUM);
-   
-       if($rows > 0){
-        $_SESSION['Username'] = $rows[0];
-        header('Location: post.php');
-        exit();   
-        }else{
-          echo  'Username or Password is incorrect ';   
-              }
-      }
-
-    function logged_in(){
-        return (isset($_SESSION['Username'])) ? true : false; 
-       }
-  
-     function posts(){
-      global $pdo;
-      $query = $pdo->prepare("SELECT * FROM `Posts`,`Users` WHERE Username = CommenterID ORDER BY `PostID` DESC");
-      $query->execute();
-      return $query->fetchAll();
-      }
+    include('php-inc/authenticate-user.php'); // this file include config and routes
     
-     function add_post($Username,$Comment,$file_parh){
-      global $pdo; 
-      if(empty($file_parh)){
-       $file_parh = 'NULL';
-      }
-      $query = $pdo->prepare('INSERT INTO `Posts` (`PostID`, `CommenterID`, `Comment`, `status_image`, `DateCommented`) VALUES (NULL, ?, ?,?,  CURRENT_TIMESTAMP)');
-      $query->bindValue(1,$Username);
-      $query->bindValue(2,$Comment);
-      $query->bindValue(3,$file_parh);
+    $has_profile = false;
+    $user_profile = false;
+    if(!empty($USER)){
+        
+        $ses_sql = mysqli_query($db,"SELECT * from UserProfiles WHERE username = '".$USER['Username']."' ");
+        $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+        if($row){
+            $has_profile = true;
+            $user_profile = $row;
+
+            $user_profile['ProfilePic'] = str_replace('../assets/', 'assets/', $user_profile['ProfilePic']);
+        }        
+        
+    }   
+
+
+
+    function posts(){
+        global $db;
+        $query = $db->query("SELECT  * FROM Posts as p LEFT JOIN UserProfiles AS up ON p.Username = up.Username ");
+     
+        if($query){
+            $result = $query->fetch_all(MYSQLI_ASSOC);
+            // var_dump($result);
+          return $result;
+        }
+        return [];
+    }
+
+    function post_comments($PostID){
+        global $db;
+        $query = $db->query("SELECT  * FROM Comments WHERE PostID = '".$PostID."' ");
+     
+        if($query){
+            $result = $query->fetch_all(MYSQLI_ASSOC);
+            // var_dump($result);
+            return $result;
+        }
+        return [];
+    }
+    
+    function add_post($Username,$Content){
+      global $db; 
+      $query = $db->prepare('INSERT INTO `Posts` (`PostID`, `Username`, `Content`, `DatePublished`, `Likes`, `Hashtag`) VALUES (NULL, ?, ?, CURRENT_TIMESTAMP, 0, NULL)');
+      $query->bind_param("ss", $Username, $Content);
       $query->execute();
-      header('Location: post.php');
-     }
+      //var_dump($query);
+      return $query->insert_id ?? 0;
+    }
+
+    function add_like_to_post($PostID){
+        global $db; 
+        $query = $db->query("UPDATE Posts SET Likes = Likes + 1 WHERE PostID = '".$PostID."'");
+        return $query;
+    }
+
+    function add_comment_to_post($Username,$PostID,$Comment){
+        global $db; 
+        $query = $db->prepare('INSERT INTO `Comments` (`PostCommentID`, `PostID`, `CommenterID`, `Comment`, `DateCommented`) VALUES (NULL, ?, ?, ?, CURRENT_TIMESTAMP)');
+        $query->bind_param("iss", $PostID, $Username, $Comment);
+        $query->execute();
+        var_dump($query);
+        return $query->insert_id ?? 0;
+    }
   
-     function user_data($Username){
-      global $pdo;
-      $query = $pdo->prepare('SELECT * FROM Users WHERE Username = ?');
-      $query->bindvalue(1,$Username);
-      $query->execute();
    
-      return $query->fetch();
-     }
-   
-     function timeAgo($time_ago){
+    function timeAgo($time_ago){
    
       $time_ago = strtotime($time_ago);
       $cur_time   = time();
@@ -142,167 +134,166 @@
       }
 
     }
-}
 
- $check = new Main;
- $get   = new Main;
- $send  = new Main;
- @$Username = $_SESSION['Username'];
- if (is_null(@$Username)){
-     echo 'Cant print variable is null';
-}
-
- $data  = $get-> user_data(@$Username);
- $post  = $get->posts();
- if(isset($_POST['submit'])){
-  $Comment  = $_POST['Comment'];
-  if (isset($_FILES['post_image'])===true) {
-    if (empty($_FILES['post_image']['name']) ===true) {
-    if(!empty($status)===true){
-     $send->add_post($Username,$Comment, $file_parh);
+ 
+    var_dump($_POST);
+    if(isset($_POST['submit']) && !empty($_POST['content'])){
+        if(add_post($USER['Username'],$_POST['content'])){
+            header("Location: post.php");
+        }
     }
-      }else {                                                                                                      
-     $allowed = array('jpg','jpeg','gif','png'); 
-     $file_name = $_FILES['post_image']['name']; 
-     $file_extn = strtolower(end(explode('.', $file_name)));
-     $file_temp = $_FILES['post_image']['tmp_name'];
-     
-     if (in_array($file_extn, $allowed)===true) {
-      $file_parh = 'images/posts/' . substr(md5(time()), 0, 10).'.'.$file_extn;
-       move_uploaded_file($file_temp, $file_parh);
-       $send->add_post($user_id,$status,$file_parh);
 
-     }else{
-      echo 'incorrect File only Allowed with less then 1mb ';
-      echo implode(', ', $allowed);
-     }
+    if(isset($_POST['like']) && !empty($_POST['PostID'])){
+        if(add_like_to_post($_POST['PostID'])){
+            header("Location: post.php");
+        }
     }
-   }
-  }
+
+    if(isset($_POST['comment']) && !empty($_POST['PostID']) && !empty($_POST['comment_content'])){
+        if(add_comment_to_post($USER['Username'], $_POST['PostID'], $_POST['comment_content'])){
+            header("Location: post.php");
+        }
+    }
 ?>
 
 <html>
  <head>
-  <title>Post on Covid Hut</title>
-  <link rel="stylesheet" href="post.css">
-  <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script> 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
- </head>
+    <title>Post on Covid Hut</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
+  
+    
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="assets/main.css">
+    <link rel="stylesheet" href="post.css">
+    
+</head>
 
 <body>
-<div class="head">
- <div class="head-in">
-  <div class="head-logo">
-   <h1 class="h-1"></h1>
-  </div>
-  <?php 
-   if($check->logged_in() === false){
-    include 'users/login.php';
-   } 
-  ?>
-</div>
-
-<div class="wrapper">  
-  <div class="content">
-   <div class="center">
-    <div class="posts">
-     <div class="create-posts">
-      <form action="" method="post" enctype="multipart/form-data">
-      <div class="c-header">
-       <div class="c-h-inner">
-        <ul> 
-         <li style="border-right:none;"><i class="fa fa-pencil-square-o" aria-hidden="true"></i><a href="#">Update Status</a></li>
-         <li><input type="file" onchange="readURL(this);" style="display:none;" name="post_image" id="uploadFile"></li>
-         <li><i class="fa fa-camera" aria-hidden="true"></i><a href="#" id="uploadTrigger" name="post_image">Add Photos/Video</a></li>
-        </ul>
-       </div>
-      </div>
-      <div class="c-body">
-       <div class="body-left">
-        <div class="img-box">
-         <img src="<?php echo $data['profile_image'];?>"></img>
-         
+    <?php include_once('php-inc/nav.php'); ?>
+    <div class="head">
+        <div class="head-in">
+            <div class="head-logo">
+                <h1 class="h-1"></h1>
+            </div>
         </div>
-       </div>
-       <div class="body-right">
-        <textarea class="text-type" name="status" placeholder="What's on your mind?"></textarea>
-       </div>
-       <div id="body-bottom">
-       <img src="#"  id="preview"/>
-       </div>
-      </div>
-      <div class="c-footer">
-       <div class="right-box">
-        <ul>
-         <li><button class="btn1"><i class="fa fa-globe" aria-hidden="true"></i>Public</button></li>
-         <li><input type="submit" name="submit" value="Post" class="btn2"/></li>
-        </ul>
-       </div>
-        
-       </div>
-      </div>
-      </div>
+    </div>
 
-  <script type="text/javascript">
-        $("#uploadTrigger").click(function(){
-           $("#uploadFile").click();
-        });
-              function readURL(input) {
-                  if (input.files && input.files[0]) {
-                      var reader = new FileReader();
-
-                      reader.onload = function (e) {
-                       $('#body-bottom').show();
-                          $('#preview').attr('src', e.target.result);
-                      }
-
-                      reader.readAsDataURL(input.files[0]);
-                  }
-              }
-   </script>
-
-<?php foreach($post as $row){
-       $time_ago = $row['status_time'];
-      echo '
-      <div class="post-show">
-         <div class="post-show-inner">
-          <div class="post-header">
-           <div class="post-left-box">
-            <div class="id-img-box"><img src="'.$row['profile_image'].'"></img></div>
-            <div class="id-name">
-             <ul>
-              <li><a href="#">'.$row['username'].'</a></li>
-              <li><small>'.$get->timeAgo($time_ago).'</small></li>
-             </ul>
-            </div>
-           </div>
-           <div class="post-right-box"></div>
-          </div>
+    <div class="wrapper">  
+        <div class="content">
+            <div class="center">
+                <div class="posts">
+                    <div class="create-posts">
+                        <form action="" method="post" enctype="multipart/form-data">
+                            <div class="c-header">
+                                <div class="c-h-inner">
+                                    <ul> 
+                                        <li style="border-right:none;"><i class="fa fa-pencil-square-o" aria-hidden="true"></i><a href="#">Update Status</a></li>
+                                        <li><input type="file" onchange="readURL(this);" style="display:none;" name="post_image" id="uploadFile"></li>
+                                        <li><i class="fa fa-camera" aria-hidden="true"></i><a href="#" id="uploadTrigger" name="post_image">Add Photos/Video</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="c-body">
+                                <div class="body-left">
+                                    <div class="img-box">
+                                        <img src="<?=$user_profile['ProfilePic']??'https://via.placeholder.com/100';?>"></img>
          
-           <div class="post-body">
-           <div class="post-header-text">
-            '.$row['status'].'
-           </div>'.( ($row['status_image'] != 'NULL') ? '<div class="post-img">
-            <img src="'.$row['status_image'].'"></img></div>' : '').'
-           <div class="post-footer">
-            <div class="post-footer-inner">
-             <ul>
-              <li><a href="#">Like</a></li>
-              <li><a href="#">Comment</a></li>
-              <li><a href="#">Share</a></li>
-             </ul> 
-            </div>
-           </div>
-          </div>
-         </div>
-        </div><br> ';}?>
+                                    </div>
+                                </div>
+                                <div class="body-right">
+                                    <textarea   textarea class="text-type" name="content" placeholder="What's on your mind?"></textarea>
+                                </div>
+                                <div id="body-bottom">
+                                    <img src="#"  id="preview"/>
+                                </div>
+                            </div>
+                            <div class="c-footer">
+                                <div class="right-box">
+                                    <ul>
+                                    <li><button class="btn1"><i class="fa fa-globe" aria-hidden="true"></i>Public</button></li>
+                                    <li><input type="submit" name="submit" value="Post" class="btn2"/></li>
+                                    </ul>
+                                </div>
+        
+                            </div>
+                        </form>
+                    </div>
+                </div>
 
+                <?php $posts  = posts(); ?>
+                <?php foreach($posts as $post): ?>
+                
+                    <div class="post-show">
+                        <div class="post-show-inner">
+                            <div class="post-header">
+                                <div class="post-left-box">
+                                    <div class="id-img-box"><img src="<?=str_replace('../assets/', 'assets/', ($post['ProfilePic']?? 'https://via.placeholder.com/100'))?>"></img></div>
+                                    <div class="id-name">
+                                        <ul>
+                                            <li><a href="#"><?=$post['Username']?></a></li>
+                                            <li><small><?=timeAgo($post['DatePublished'])?></small></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="post-right-box"></div>
+                            </div>
+                            
+                            <form action="" method="post" class="" enctype="multipart/form-data">
+                                <div class="post-body">
+                                    <div class="post-header-text"><?=$post['Content']??''?></div>
+                                    
+                                    <?php if( !empty($post['status_image']) ) : ?> 
+                                        <!-- this is not used what is status_image ??? -->
+                                        <div class="post-img">
+                                            <img src="<?=$post['status_image']?>"/>
+                                        </div>
+                                    <?php endif; ?>
+                                    
+                                    <?php $post_comments = post_comments($post['PostID']); ?>
+                                    <?php if( !empty($post_comments) ) : ?> 
+                                        <div class="comments">
+                                            <?php foreach($post_comments as $comment): ?>
+                                                <div class="a-comment">
+                                                    <div class="commnet-text"><?=$comment['Comment']?></div>
+                                                    <small class="commenter-username"><?=$comment['CommenterID']?></small>
+                                                    <small class="comment-date"><?=$comment['DateCommented']?></small>
+                                                </div>
+                                            <?php endforeach; ?>
+                                        </div>
+                                    <?php endif; ?>
+                                
+                                    <input type="hidden" name="PostID" value="<?=$post["PostID"]?>">
+                                    <textarea class="text-type" name="comment_content" placeholder="Comment"></textarea>
+                                    
+
+                                    <div class="post-footer">
+                                        <div class="post-footer-inner">
+                                            <ul>
+                                                <li><input type="submit" name="like" value="Likes (<?=$post['Likes']??0?>)" class="btn2"/></li>
+                                                <li><input type="submit" name="comment" value="Comment" class="btn2"/></li>
+                                                <li><a href="#">Share</a></li>
+                                            </ul> 
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <br>
+                <?php endforeach; ?>
+
+            </div>
+        </div>
     </div>
-    </form>           
-    </div>
+
+    
 </body>
 </html>
